e9b06a453b7dffb0f0983ddfa5883c422a4d2409
SashRav <sasha_ravinsky@tut.by>
2021-02-21 21:21:30 +0300

    Adding UE_LOG into the BaseGeometryActor class

--- Source/GSandbox/Private/BaseGeometryActor.cpp
+++ Source/GSandbox/Private/BaseGeometryActor.cpp
@@ -16,6 +16,8 @@ void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	//printTypes();
+	printTypes();
 }
 
 // Called every frame
@@ -23,5 +25,24 @@ void ABaseGeometryActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ABaseGeometryActor::printTypes()
+{
+	UE_LOG(LogTemp, Display, TEXT("Hello Unreal!"));
+	UE_LOG(LogTemp, Warning, TEXT("Hello Unreal!"));
+	UE_LOG(LogTemp, Error, TEXT("Hello Unreal!"));
+
+	int WeaponsNum = 4;
+	int killsNum = 7;
+	float Health = 34.5253f;
+	bool isDead = false;
+	bool hasWeapon = true;
+
+	UE_LOG(LogTemp, Display, TEXT("Wearpons num: %d, kills num: %i"), WeaponsNum, killsNum);
+	UE_LOG(LogTemp, Display, TEXT("Health: %f"), Health);
+	UE_LOG(LogTemp, Display, TEXT("Health: %.2f"), Health);
+	UE_LOG(LogTemp, Display, TEXT("Is dead: %d"), isDead);
+	UE_LOG(LogTemp, Display, TEXT("Has wearpon: %d"), static_cast<int>(hasWeapon));
 }
 
